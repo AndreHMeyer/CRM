2fe57e61ce1c37c4bdec09ff448a02acdb9a5273
Ed2002 <ed49516@gmail.com>
2023-07-03 00:32:48 -0300

    Total Pages

--- Domain/Pagination/PaginationService.cs
+++ Domain/Pagination/PaginationService.cs
@@ -11,6 +11,22 @@ namespace Domain.Pagination
             result.RegistersPerPage = pag.PageSize;
             result.Page = pag.Page;
             result.Total = data.Count();
+            var t = result.Total;
+            var tp = 0;
+            while(t > 0)
+            {
+                if(t < pag.PageSize)
+                {
+                    t = 0;
+                    tp++;
+                }
+                else
+                {
+                    t -= pag.PageSize;
+                    tp++;
+                }
+            }
+            result.TotalPages = tp;
             result.Values = data.Skip((pag.Page - 1)* pag.PageSize).Take(pag.PageSize).ToList();
             return new Result<PaginationResult<T>>().CreateSucess(result);
         }
